cad053a444f7cfe5fc6a630ef37944f490896315
Adam Silverthorne <dooger@gmail.com>
2020-03-25 16:46:38 -0700

    package

---
+++ package.js
@@ -0,0 +1,12 @@
+Package.describe({
+    name: 'recurring',
+    summary: 'Recurly API 2.0',
+    version: '2.7.0'
+});
+
+Package.onUse(function(api) {
+    api.mainModule('lib/index.js', 'server');
+    api.export('recurring', 'server');
+
+    api.use(['modules'], 'server');
+});
